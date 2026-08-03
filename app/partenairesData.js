7d31d393ab4890ba16f49f8f17849f6d8157f86b
myopostoie <bahsabinkoffi@gmail.com>
2026-08-03 15:04:57 +0200

    Add partenairesList with partner data

---
+++ app/partenairesData.js
@@ -0,0 +1,22 @@
+export const partenairesList = [
+  {
+    identifiant: "+22997000000", // ou l'email
+    codeRc: "RC001",
+    nom: "Koffi Mensah",
+    niveau: "Partenaire Or",
+    ventes: 12,
+    chiffreAffaires: "450 000 FCFA",
+    commission: "90 000 FCFA",
+    motivation: "Excellent travail ! Tu es l'un des meilleurs vendeurs ce mois-ci. Continue comme ça !"
+  },
+  {
+    identifiant: "+22507000000",
+    codeRc: "RC002",
+    nom: "Awa Diallo",
+    niveau: "Partenaire Argent",
+    ventes: 5,
+    chiffreAffaires: "180 000 FCFA",
+    commission: "36 000 FCFA",
+    motivation: "Bienvenue dans l'équipe ! Tes efforts portent leurs fruits, objectif 10 ventes cette semaine !"
+  }
+];
