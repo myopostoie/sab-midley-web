5bd0eb1cb191291f37e6ffa692558cc7499046e6
myopostoie <bahsabinkoffi@gmail.com>
2026-08-05 09:59:02 +0200

    Update partenairesData.js

--- app/partenairesData.js
+++ app/partenairesData.js
@@ -1,5 +1,35 @@
 export const partenairesList = [
   {
+    identifiant: "+22996580081",
+    codeRc: "RC123",
+    nom: "Alex BAH",
+    niveau: "Partenaire Or",
+    ventes: 12,
+    chiffreAffaires: "450 000 FCFA",
+    commission: "90 000 FCFA",
+    motivation: "Excellent travail ! Tu es l'un des meilleurs vendeurs ce mois-ci. Continue comme ça !"
+  },
+  {
+    identifiant: "+22507000000",
+    codeRc: "RC002",
+    nom: "Awa Diallo",
+    niveau: "Partenaire Argent",
+    ventes: 5,
+    chiffreAffaires: "180 000 FCFA",
+    commission: "36 000 FCFA",
+    motivation: "Bienvenue dans l'équipe ! Tes efforts portent leurs fruits, objectif 10 ventes cette semaine !"
+  },
+  {
+    identifiant: "+22990000000",
+    codeRc: "RC999",
+    nom: "Marc KOUTON",
+    niveau: "Partenaire Débutant",
+    ventes: 0,
+    chiffreAffaires: "0 FCFA",
+    commission: "0 FCFA",
+    motivation: "Bienvenue dans l'aventure ! C'est le moment idéal pour faire ta première vente. Consulte les canaux et publie tes premières offres !"
+  },
+  {
     identifiant: "+22948817396",
     codeRc: "RCQLDACC1",
     nom: "Damaas TONABSE",
@@ -10,7 +40,7 @@ export const partenairesList = [
     motivation: "Bienvenue dans l'aventure ! C'est le moment idéal pour faire ta première vente."
   },
   {
-    identifiant: "+22812BENIN", // ou numéro partiel extrait
+    identifiant: "+22812",
     codeRc: "RCBMCTOM",
     nom: "ADANHOUNME Harm",
     niveau: "Partenaire Débutant",
