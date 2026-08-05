8254c7256c260f207430a09324f72bec3018ea30
Me Koffi <ceo@sabmidley.co>
2026-08-05 11:41:36 +0200

    Correction du chemin d'importation products

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { useState, useEffect } from 'react';
-import { productsList as initialProducts } from './products';
-import jsPDF from 'jspdf';
+import { productsList as initialProducts } from '../../../products';import jsPDF from 'jspdf';
 
 export default function Home() {
   const [productsList, setProductsList] = useState<any[]>(initialProducts);
